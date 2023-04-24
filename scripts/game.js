81191c3044a3a48dcdc65022c1289d86d7d79a4a
Joao Madeira <129863118+jodmadeira@users.noreply.github.com>
2023-04-24 18:09:48 +0100

    inimigos com lvls

--- scripts/game.js
+++ scripts/game.js
@@ -36,7 +36,7 @@ class Game {
         // Updating Frames
         this.frames ++; // 100 * 10 = 1s ; 200 * 10 = 2s
         // Updating Score
-        this.score += 5;
+        this.score += 1;
         // Updating Lives
         this.updateLevel();
         this.lives;
@@ -94,44 +94,79 @@ class Game {
 
     
     updateEnemies(){
-        //Enemies Postions
-        let gap = 80;
-        let maxWidth = rightLimit-leftLimit - gap; // max width of 400px
-
-
+        //Enemies Starting Positions
         let starterHeight = 40;
-        let randomX = leftLimit + Math.floor(Math.random()*(rightLimit-leftLimit));
+        let randomX = Math.floor(Math.random()*(rightLimit-leftLimit));
         
         
         //List of Enemies
-        const policeCar = new Component(leftLimit,starterHeight,100,100, true,'../images/police-car.png.png',ctx);
-        const helicopter = new Component(leftLimit,starterHeight,100,100, true,'../images/helicopter.gif',ctx);
-
-        const enemiesList = [policeCar, helicopter];
+        const policeCar = new Component(leftLimit,starterHeight,100,40, true,'../images/police-car.png.png',ctx);
+        const helicopter = new Component(leftLimit,starterHeight,100,40, true,'../images/helicopter.gif',ctx);
         
+        //checkpoint Enemy
+        const barrierL = new Component(leftLimit,starterHeight,200,20,true,'../images/cops-car.png',ctx);
+        const barrierR = new Component(rightLimit-200,starterHeight,200,20,true,'../images/cops-car.png',ctx);
+        
+        const enemiesList = [policeCar, helicopter,policeCar, policeCar, policeCar];
+       
+        // Enemies movement
         for (let i = 0; i<this.enemies.length; i++){
             // enemies's y ++
             this.enemies[i].y +=1*this.level; 
-            // enemies' draw function
-            this.enemies[i].draw();
+             // enemies' draw function
+             this.enemies[i].draw();
+            
         }
-        
-        
-        // each 2 seconds, a enemy is updated
-        if(this.frames % 200 === 0){
-        
-        // Choose a random enemy from the Array of possible enemies
-        let randomEnemy = Math.floor(Math.random()*enemiesList.length)
-        
-        // Give a random position to the enemy
-        enemiesList[randomEnemy].x +=randomX
-        // Push enemy into enemy array
-        this.enemies.push(enemiesList[randomEnemy]);
+        //level 1 difficulty from Start to 28seconds
+        if(this.frames < 2801){
+            
+            // each 2 seconds, a enemy is added to the array
+            if(this.frames % 200 === 0){
+                let randomEnemy = Math.floor(Math.random()*enemiesList.length)
+                // Give a random position to the enemy
+                enemiesList[randomEnemy].x +=randomX
+                // Push enemy into enemy array
+                this.enemies.push(enemiesList[randomEnemy]);
+            }
         }
+        // Level 1 final boss at 30 seconds
+        if(this.frames % 3000 ===0) {
+            this.enemies.push (barrierL, barrierR)
+        }
+
+        //level 2 difficulty
+        if(this.frames>3100 && this.frames<5800){
+            if(this.frames % 200 === 0){
+                let randomEnemy = Math.floor(Math.random()*enemiesList.length)
+                // Give a random position to the enemy
+                enemiesList[randomEnemy].x +=randomX
+                // Push enemy into enemy array
+                this.enemies.push(enemiesList[randomEnemy]);
+            }
+        }
+
+        // Level 2 final boss at 30 seconds
+        if(this.frames % 6000 ===0) {
+           this.enemies.push (barrierL, barrierR)
+        }
+
+        //level 3 difficulty
+        if(this.frames>6100 && this.frames<8800){
+            if(this.frames % 200 === 0){
+                let randomEnemy = Math.floor(Math.random()*enemiesList.length)
+                // Give a random position to the enemy
+                enemiesList[randomEnemy].x +=randomX
+                // Push enemy into enemy array
+                this.enemies.push(enemiesList[randomEnemy]);
+            }
+        }
+        // Level 2 final boss at 30 seconds
+        if(this.frames % 9000 ===0) {
+            this.enemies.push (barrierL, barrierR)}
     }
 
     checkWin(){
-        if(this.level===2){
+        if(this.level===4){
             ctx.fillStyle = 'black';
             ctx.fillRect(100, 100, 400, 250);
             ctx.font = '32px Helvetica';
